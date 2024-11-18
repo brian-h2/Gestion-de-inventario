9217bfabaf5b38d8df52684b9d77781bf3fa2c7f
brian-h2 <brianheredia200309@gmail.com>
2024-11-18 20:56:21 -0300

    Feat Create new endpoint Get and Post for create Tasks

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using ApiMinimal.Context;
+using ApiMinimal.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -18,5 +19,27 @@ app.MapGet("/dbconexion", async ([FromServices] ApiMinimalContext dbContext) =>
     dbContext.Database.EnsureCreated();
     return Results.Ok("Database in memory" + dbContext.Database.IsInMemory());
 });
+
+app.MapGet("/itemsAll", async (ApiMinimalContext apidb) =>
+{
+    var items = await apidb.Tasks.ToListAsync();
+    return Results.Ok(items);
+});
+
+app.MapPost("/createTask", async (Tasks task, ApiMinimalContext dbcontext) =>
+{
+    dbcontext.Tasks.Add(task);
+    await dbcontext.SaveChangesAsync();
+    return Results.Created($"/tasks/{task.IdTask}", task);
+
+});
+
+app.MapGet("/{name}", async (string name, ApiMinimalContext apidb) =>
+{
+    var itemsResult = await apidb.Tasks.FirstOrDefaultAsync(x => x.TaskName == name);
+    return Results.Ok(itemsResult);
+});
+
+
 
 app.Run();
